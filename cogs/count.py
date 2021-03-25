7786dd8c5974ddc2e174bfc10155c694be134a3d
daima3629 <daima3629@usbc.be>
2021-03-25 17:39:40 +0900

    [Add] メンバー入室・退室時の処理を追加

--- cogs/count.py
+++ cogs/count.py
@@ -11,6 +11,14 @@ class CountCog(commands.Cog):
         count = self.count_map.get(ctx.author.id)
         await ctx.send(f"あなたのカウント: {count}")
 
+    @commands.Cog.listener()
+    async def on_member_join(self, member) -> None:
+        await self.db.add_user(member.id)
+
+    @commands.Cog.listener()
+    async def on_member_remove(self, member) -> None:
+        await self.db.remove_user(member.id)
+
 
 def setup(bot):
     bot.add_cog(CountCog(bot))
