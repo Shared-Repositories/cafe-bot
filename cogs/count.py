5bdf5115124847ee966d441430d24374dd905292
daima3629 <daima3629@usbc.be>
2021-03-25 17:55:32 +0900

    [Add&Fix] メッセージでカウントアップする処理を追加&typo修正

--- cogs/count.py
+++ cogs/count.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 class CountCog(commands.Cog):
     def __init__(self, bot) -> None:
         self.bot = bot
-        self.count_map = await self.db.get_count_map()
+        self.count_map = await self.bot.db.get_count_map()
 
     @commands.command()
     async def mycount(self, ctx) -> None:
@@ -13,13 +13,23 @@ class CountCog(commands.Cog):
 
     @commands.Cog.listener()
     async def on_member_join(self, member) -> None:
-        await self.db.add_user(member.id)
+        await self.bot.db.add_user(member.id)
         self.count_map[member.id] = 0
 
     @commands.Cog.listener()
     async def on_member_remove(self, member) -> None:
-        await self.db.remove_user(member.id)
+        await self.bot.db.remove_user(member.id)
         self.count_map.pop(member.id)
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.author.bot:
+            return
+        if message.channel.id == 779693431835721728:
+            return
+
+        await self.bot.db.count_up(message.author.id)
+        self.count_map[message.author.id] += 1
 
 
 def setup(bot):
