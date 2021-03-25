1853939215176a223acec8c8530ce1d86423c347
daima3629 <daima3629@usbc.be>
2021-03-25 17:34:32 +0900

    [Add] mycountコマンドを追加

--- cogs/count.py
+++ cogs/count.py
@@ -4,6 +4,12 @@ from discord.ext import commands
 class CountCog(commands.Cog):
     def __init__(self, bot) -> None:
         self.bot = bot
+        self.count_map = await self.db.get_count_map()
+
+    @commands.command()
+    async def mycount(self, ctx) -> None:
+        count = self.count_map.get(ctx.author.id)
+        await ctx.send(f"あなたのカウント: {count}")
 
 
 def setup(bot):
