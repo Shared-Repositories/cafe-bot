d15d9be6f21267bc3b57f83debc729c3d3de7fd2
daima3629 <daima3629@usbc.be>
2021-03-25 18:29:50 +0900

    [Add] 週の初めにロールを追加・剥奪する処理を追加

--- cogs/count.py
+++ cogs/count.py
@@ -1,10 +1,12 @@
-from discord.ext import commands
+from discord.ext import commands, tasks
+import time
 
 
 class CountCog(commands.Cog):
     def __init__(self, bot) -> None:
         self.bot = bot
         self.count_map = await self.bot.db.get_count_map()
+        self.count_task.start()
 
     @commands.command()
     async def mycount(self, ctx) -> None:
@@ -30,6 +32,30 @@ class CountCog(commands.Cog):
 
         await self.bot.db.count_up(message.author.id)
         self.count_map[message.author.id] += 1
+
+    @tasks.loop(seconds=60)
+    async def count_task(self):
+        await self.bot.wait_until_ready()
+        now = time.localtime()
+        check = all(
+            now.tm_wday == 0,
+            now.tm_hour == 0,
+            now.tm_min == 0
+        )
+        if not check:
+            return
+
+        guild = self.bot.get_guild(778297702781419520)
+        role = guild.get_role(808714730998988810)
+        for member in role.members:
+            await member.remove_roles(role)
+        for user_id, count in self.count_map.items():
+            if count >= 200:
+                member = guild.get_member(user_id)
+                await member.add_roles(role)
+            self.count_map[user_id] = 0
+        await self.bot.db.reset_count()
+
 
 
 def setup(bot):
